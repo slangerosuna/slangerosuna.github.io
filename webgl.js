731a6c28f64a0ff7cd038764cdfed58f94afc63f
slangerosuna <70410350+slangerosuna@users.noreply.github.com>
2023-05-15 11:45:13 -0700

    safaa

--- webgl.js
+++ webgl.js
@@ -231,11 +231,12 @@ void main(void) {
     );
 
     var UIObjects = [];
-    UIObjects.push(
+    objects.push(
         {
             'buffers': await initBuffers(gl, "quad.obj"),
             'image': loadTexture(gl, "amoogus.png"),
-            'position': [0, 0.7]
+            'position': [0, 0.7, 0],
+            'rotation': [0, 0, 0]
         }
     );
 
